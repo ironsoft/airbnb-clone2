6d0bb4c10afd125758381db998ee75da782d36be
ishopfloor <ishopfloor@ishopfloor.com>
2022-07-15 07:53:45 +0900

    final with postgresql

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
 
     DATABASES = {
         "default": {
